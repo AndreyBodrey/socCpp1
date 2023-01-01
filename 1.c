8edc5bb0160608c304111f4b4f031fdf6ccce7d2
AndreyBodrey <astradiski@ya.ru>
2023-01-01 13:49:24 +0300

    add bind

--- 1.c
+++ 1.c
@@ -4,11 +4,22 @@
 #include <linux/if_ether.h> 
 #include <net/ethernet.h>
 #include <netinet/in.h>
+#include <strings.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main ()
 {
 	printf("heloo nigga !\n\n");
 	int soc = -1;
+	struct sockaddr_in addrLocal;
+	char *ipAdr = "192.168.0.112";   //hardcoded sheet, you must change to your IP
+
+	bzero(&addrLocal, sizeof(addrLocal));
+	addrLocal.sin_family = AF_PACKET;
+	addrLocal.sin_port = 0;
+	addrLocal.sin_addr.s_addr = inet_addr(ipAdr);
+
 
 	soc = socket(  AF_PACKET, SOCK_RAW, htons(ETH_P_ALL) );   // works only whith root user
 
@@ -18,10 +29,18 @@ int main ()
 		printf("error to cerate socket!\n");
 		return 1;
 	}
-	
-	
+	// now bind to local IP
+
+	///bind(soc, &addrLocal, sizeof(addrLocal));
+	if ( bind(soc, (struct sockaddr*)&addrLocal, sizeof(addrLocal)) != 0)
+	{		
+		printf ("bind() error!");
+		close(soc);
+		return 1;
+	}
 
 	printf("over\n");
+	
 	close(soc);
 
 }
