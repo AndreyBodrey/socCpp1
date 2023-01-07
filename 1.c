9c580d1e27c70574e428604f4d2b8b98007afc3e
AndreyBodrey <astradiski@ya.ru>
2023-01-07 11:00:06 +0300

    add functions findNetCardName, and seachIP

--- 1.c
+++ 1.c
@@ -22,14 +22,19 @@
 
 int main ()
 {
-	char buf[PACK_BUF_LEN] = {0,};
+	char ipLockalStrFormat[20];
+	uint32_t ipLockalInetFormat = serchIP(ipLockalStrFormat);
+	 // допустим айпи мы нашли и получили
 
+	
+	char buf[PACK_BUF_LEN] = {0,};
+	
 	struct ehter_header * ehH = (struct ehter_header *) buf;
 	
 	struct iphdr * ipH = (struct iphdr *)(buf + 14);
 
 
-	int countLoop = 200;
+	int countLoop = 20000;
 	printf("heloo nigga !\n\n");
 	int soc = -1;
 	struct sockaddr_in addrLocal;
@@ -51,11 +56,14 @@ int main ()
 	}
 
 			//enp4s0 spirovo
-	int rc = setsockopt(soc,SOL_SOCKET, SO_BINDTODEVICE,"enp7s0\x00", strlen("enp7s0\x00") + 1);
-	printf ("%d \n" , rc);
-	strlen(" ");
+	char netCardName[20] = {0,};
+	findNetCardName(netCardName);
+	int rc = setsockopt(soc,SOL_SOCKET, SO_BINDTODEVICE, netCardName, strlen(netCardName) + 1);
 
-	/*						// раскоментировать это для  отлавливание всех пакетов даже не для нашего мака
+	printf ("setSock Opt %d, error %d \n" , rc, errno);
+
+
+	/*	// раскоментировать это для  отлавливание всех пакетов даже не для нашего мака
 	struct ifreq interface;
 	interface.ifr_flags |= IFF_PROMISC;
 	errno = 0;
@@ -67,20 +75,14 @@ int main ()
 	}
 	*/
 	
-	int reciveBytes = 0;
+
 	while (countLoop--)
 	{
-		reciveBytes = 0;
+		int reciveBytes = 0;
 		reciveBytes = recvfrom(soc,buf,sizeof(buf),0,0,0);
 		// 20 минимальный размер пакета
 		if (reciveBytes > 20 && reciveBytes < PACK_BUF_LEN)	packetHandler(buf, reciveBytes);
 	}
-
-
-
-
-
-
 
 
 	printf("over\n");	
@@ -99,6 +101,7 @@ void packetHandler(char *buf, int bufLen)
 	if (!typeProtocol) return clearBuf(buf);
 
 	if (typeProtocol == IPPROTO_IGMP) checkIgmp(buf, bufLen);
+	if (typeProtocol == IPPROTO_UDP)  checkUdp(buf,bufLen);
 
 
 }
@@ -107,8 +110,15 @@ void packetHandler(char *buf, int bufLen)
 void checkIgmp(char *buf, int bufLen)
 {
 	//  if qwery for us, then send addmembership
+	printf("ressived IGMP pack \n");
 }
+//-------------------------------------------------------------------------------------
 
+void checkUdp(char *buf, int bufLen)
+{
+	//   checking dup packets here
+//	printf("ressived UDP pack \n");
+}
 //-------------------------------------------------------------------------------------
 
 int packFiltr(char * bufer, int len)
@@ -136,28 +146,89 @@ void clearBuf(char * buff)
 }
 //----------------------------------------------------------------------
 
-char * serchIP()
+uint32_t serchIP(char * ipLoc)
 {
-	/* 
-	// вот эот кусок получает список локальных айпи адесов
+	const int MAX_IFR = 20;
+	
+	// вот эот кусок получает список локальных айпи адесов и отсеивает не нужный 127.х.х.х. типа локадхост
 	// работает не удалать!!!
-  int i,n,s;
-  struct ifreq ifr[100];
+	// надо тестить на конечнов устройстве
+  
+  struct ifreq ifr[MAX_IFR];
   struct ifconf ifconf;
   struct sockaddr_in *sin;
- 
-  s=socket(AF_INET, SOCK_DGRAM, 0);
-  ifconf.ifc_len=sizeof(ifr);
-  ifconf.ifc_req=ifr;
-  ioctl(s, SIOCGIFCONF, &ifconf);
-  close(s);
- 
-  n=ifconf.ifc_len/sizeof(struct ifreq);
-  for(i=0;i<n;i++) {
-    sin=(struct sockaddr_in *) (&ifr[i].ifr_addr);
-    printf("%s -> %s\n",ifr[i].ifr_name,inet_ntoa(sin->sin_addr));
-  }
-	
-	*/
 
+  int sock = socket(AF_INET, SOCK_DGRAM, 0);
+  ifconf.ifc_len = sizeof(ifr);
+  ifconf.ifc_req = ifr;
+  ioctl(sock, SIOCGIFCONF, &ifconf);
+  close(sock); 
+  int n = ifconf.ifc_len / sizeof(struct ifreq);
+  for(int i = 0; i < n; i++) 
+  {
+    sin = (struct sockaddr_in *) (&ifr[i].ifr_addr);
+
+	// приведение к типу указатель на первый байт айпишника и проверка не локалхост ли он
+	//тут надо написать проверку на принадлежнаость нужному диапазону айпи что выбрать привильный
+	uint8_t *b = (uint8_t *) &(sin->sin_addr.s_addr);	
+	if (*b == 127) continue;
+    // printf("found network interface = %s -> %s\n",ifr[i].ifr_name,inet_ntoa(sin->sin_addr));
+
+	char * temp = inet_ntoa(sin->sin_addr);
+	//преписывем в строку айпи для возврата в виде строки
+	memcpy(ipLoc, temp, strlen(temp)+1);
+  }
+	//возвращаем йапи в типе int
+  return sin->sin_addr.s_addr;
+	
+
+//var2 not tested
+/* 
+#include   "unp.h"
+ #include    <sys/utsname.h>
+ char **  my_addrs(int *addrtype)
+ {
+    struct hostent *hptr;
+    struct utsname myname;
+    if (uname(&myname) < 0)    return (NULL);
+ if ( (hptr = gethostbyname(myname.nodename)) == NULL)
+ return (NULL);
+  *addrtype = hptr->h_addrtype;
+   return (hptr->h_addr_list);
+ }
+ */
+
+}
+
+//------------------------------------------------------------------------------------
+
+void igmpJoy()
+{
+	//sending igmp membership report
+	
+}
+//-------------------------------------------------------------------------------------
+
+void findNetCardName(char * name)
+{
+	struct if_nameindex *ni;
+    int i, selItem, choisenFlag = 0;
+    ni = if_nameindex();
+
+    if (ni == NULL) {
+        perror("if_nameindex()");
+        return;
+    }
+
+    for (i = 0; ni[i].if_index != 0 && ni[i].if_name != NULL; i++)
+	{
+		printf("%d: %s\n", ni[i].if_index, ni[i].if_name);
+		if ( strcmp((ni[i].if_name), "lo"))  
+			if (!choisenFlag)
+			{
+			selItem = i;
+			choisenFlag = 1;
+			}					//выбирает первую что не localhost 127.0.0.1	
+	}
+	strcpy(name, (ni[selItem].if_name));	// переносим имя сетевухи в выходнуй массив
 }
--- main.h
+++ main.h
@@ -9,3 +9,9 @@ int packFiltr(char * bufer, int len);
 
 void checkIgmp(char *buf, int bufLen);
 void checkUdp(char *buf, int bufLen);
+
+
+void checkUdp(char *buf, int bufLen);
+uint serchIP(char * );
+void igmpJoy();
+void findNetCardName(char * name);
